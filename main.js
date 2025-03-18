3d26307c840afc881fc8b2dcc4fb419850fe1417
Vimala S <vimala.sellappan@stud.sundsgarden.se>
2025-03-18 23:39:43 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let number_1=10;
+let number_2=10;
+console.log(`The addition of two nos is ${number_1+number_2}`);
+console.log(`The subtraction of two nos is ${number_1-number_2}`);
+console.log(`The Multiplication of two nos is ${number_1*number_2}`);
+console.log(`The Division of two nos is ${number_1/number_2}`);
